f440ead8762d802deb983d07831f9aaee6f39f4b
Rimmert Zelle <rimmertzelle@ip87-150.HZeeland.nl>
2023-04-03 10:11:24 +0200

    added tailwindcss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import "../app.css";
+</script>
+
+<div class="flex flex-col h-screen justify-between">
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
+
+<style>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<div class="container w-full mx-auto">
+    <h1>Welcome to Sveltekit</h1>
+</div>
+
+<style>
+</style>
