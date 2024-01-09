3860cb150d5259271cf2253b91dbaf4697eac47b
Jens Joosse <joos0067@hz.nl>
2024-01-09 17:22:24 +0100

    commit 9-1-24 17:22

--- src/routes/challenges/+page.svelte
+++ src/routes/challenges/+page.svelte
@@ -10,24 +10,27 @@
     <div class="content">
         <div class="w-full h-full flex items-center justify-center mt-8">
             <div class="w-4/5 h-4/5 bg-white">
-                <div class="w-full h-1/4 font-inter">
-                    challenges
+                <div class="flex justify-center">
+                    <div class="h-1/4 font-inter text-3xl">
+                        challenges
+                    </div>
                 </div>
-                <div class="w-full bg-indigo-800 h-12">
-
+                <div class="w-full bg-indigo-800 h-12 text-white">
+                    Todays date ------------- finished?
                 </div>
-                <div class="w-full bg-slate-200 h-96 justify-center flex space-y-10 mb-10 text-white">
-                    <div class="w-11/12 bg-indigo-800 h-5 space-y-10 mt-5">
+                <div class="m-5 bg-slate-200 justify-center flex mb-10 text-white">
+                    
+                    <div class="w-full bg-indigo-800 h-5 ">
                         Day 1
                     
                     <div class="w-full bg-blue-100 justify-center">
-                        <div class="bg-indigo-800 w-11/12 h-16 m-5">
+                        <div class="bg-indigo-500  h-16 m-3">
                             challenge 1
                         </div>
-                        <div class="bg-indigo-800 w-11/12 h-16 m-5">
+                        <div class="bg-indigo-500  h-16 m-3">
                             challenge 2
                         </div>
-                        <div class="bg-indigo-800 w-11/12 h-16 m-5">
+                        <div class="bg-indigo-500 h-16 m-3">
                             challenge 3
                         </div>
                     </div>
