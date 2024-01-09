d893b016a349d5f7d3fc9adda49515772261471e
Rienzi <spad0001@hz.nl>
2024-01-09 12:05:03 +0100

    Created the navbar and the profile page

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,42 @@
+<script src="./app.js">
+</script>
+
+<head>
+    <!-- Other head elements -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <title>Your Page Title</title>
+</head>
+<nav class="bg-[#3730A3] p-4 sticky top-0 w-full z-10">
+    <div class="container mx-auto flex justify-between items-center">
+        <a href="/home" class="text-[#DFC2F2] text-6xl font-bold">Anxietr</a>
+
+        <ul class="flex space-x-4 items-center">
+            <li>
+                <a
+                    href="/home"
+                    class="text-[#DFC2F2] text-2xl font-bold hover:text-white">
+                    <i class="fas fa-home"></i>
+                </a>
+                <a
+                    href="/chat"
+                    class="text-[#DFC2F2] text-2xl p-8 font-bold hover:text-white">
+                    <i class="fas fa-users"></i>
+                </a>
+                <a 
+                    href="/activities"
+                    class="text-[#DFC2F2] text-2xl p-8 font-bold hover:text-white">
+                    <i class="fas fa-comments"></i> 
+                </a>
+                <a
+                    href="/profilepage"
+                    class="text-[#DFC2F2] text-2xl font-bold hover:text-white">
+                    <i class="fas fa-user"></i></a>
+                <i class='fa-solid fa-moon text-[#DFC2F2] text-2xl font-bold hover:text-white'></i>
+                <i class='fa-solid fa-sun text-[#DFC2F2] text-2xl font-bold hover:text-white'></i>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<style>
+</style>
---
+++ src/routes/profilepage/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    import NavBar from "$lib/components/NavBar.svelte";
+  </script>
+  
+  <svelte:head>
+    <title>User Profile</title>
+    <meta name="description" content="User Profile Page" />
+  </svelte:head>
+  
+  <header>
+    <NavBar />
+  </header>
+  
+  <body class="bg-white h-screen" style="grid:1fr; justify-items: center">
+  
+    <div class="bg-#f1f2f6]" style="grid-template-rows: 500px auto;">
+  
+      <div class="w-[80%]" style="grid-template-rows: 450px 50px;">
+          <div class="bg-purple-50 w-[100%]">
+              <a href="https://imgbb.com/"><img src="https://i.ibb.co/1dtpJxp/zfKMEfC.jpg" alt="zfKMEfC" border="0"></a>
+          </div>
+          <div></div>
+      </div>
+      <div></div>
+    </div>
+  
+  </body>
+  
+  <style>
+  </style>
