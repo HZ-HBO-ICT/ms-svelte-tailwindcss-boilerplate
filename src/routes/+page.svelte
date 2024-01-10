fa3f6ba1bf93f7a72ccb6731b616a356055482cb
Preciousgift Amazu <amazuchizurumoke@gmail.com>
2024-01-10 12:15:37 +0100

    switching between login and signup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,24 @@
-
 <script>
    // @ts-ignore
    import NavBar from "$lib/components/NavBar.svelte";
+   import { onMount } from "svelte";
+
+  let showSignup = true;
+
+  // Function to toggle between signup and login
+  function toggleView() {
+    showSignup = !showSignup;
+  }
+
+  // Scroll to the top of the page when the component is mounted
+  onMount(() => {
+    window.scrollTo(0, 0);
+  });
   </script>
+
   <svelte:head>
     <title>welcome</title>
-    <meta name="description" content="welcom" />
+    <meta name="description" content="welcome" />
   </svelte:head>
   
   <header>
@@ -23,15 +36,33 @@
         </div>
       </div>
 
-      <div class="signup w-[474px] h-[600px] bg-white rounded-[10px] shadow border border-slate-100 grid place-items-center">
-        <div class="text-center text-indigo-800 text-5xl font-semibold font-['Inter']">Sign up</div>
-        <input class="w-[389px] h-[65px] bg-slate-100 rounded-[10px] shadow border-2 border-slate-100" type="text" placeholder="Username">
-        <input class="w-[389px] h-[65px] bg-slate-100 rounded-[10px] shadow border-2 border-slate-100" type="text" placeholder="Email">
-        <input class="w-[389px] h-[65px] bg-slate-100 rounded-[10px] shadow border-2 border-slate-100" type="text" placeholder="Password">
-        <input class="w-[389px] h-[65px] bg-slate-100 rounded-[10px] shadow border-2 border-slate-100" type="text" placeholder="Confirm Password">
-        <button class="w-[389px] h-[65px] bg-indigo-800 rounded-[10px] shadow text-white text-2xl font-bold font-['Inter']">Sign up</button>
-      </div>
-      
+     <div>
+        <div class="contain flex justify-around">
+            <div>
+              {#if showSignup}
+                <!-- Signup Form -->
+                <div class="signup w-[474px] h-[600px] bg-white rounded-[10px] shadow border border-slate-100 grid place-items-center">
+                    <div class="text-center text-indigo-800 text-5xl font-semibold font-['Inter']">Sign up</div>
+                    <input class="w-[389px] h-[65px] bg-slate-100 rounded-[10px] shadow border-2 border-slate-100" type="text" placeholder="Username">
+                    <input class="w-[389px] h-[65px] bg-slate-100 rounded-[10px] shadow border-2 border-slate-100" type="text" placeholder="Email">
+                    <input class="w-[389px] h-[65px] bg-slate-100 rounded-[10px] shadow border-2 border-slate-100" type="text" placeholder="Password">
+                    <input class="w-[389px] h-[65px] bg-slate-100 rounded-[10px] shadow border-2 border-slate-100" type="text" placeholder="Confirm Password">
+                    <button class="w-[389px] h-[65px] bg-indigo-800 rounded-[10px] shadow text-white text-2xl font-bold font-['Inter']">Sign up</button>
+                  <p>Already have an account? <a  on:click={toggleView}>Switch to Login</a></p>
+                </div>
+              {:else}
+                <!-- Login Form -->
+                <div class="login w-[474px] h-[600px] bg-white rounded-[10px] shadow border border-slate-100 grid place-items-center">
+                    <div class="text-center text-indigo-800 text-5xl font-semibold font-['Inter']">Log In</div>
+                    <input class="w-[389px] h-[65px] bg-slate-100 rounded-[10px] shadow border-2 border-slate-100" type="text" placeholder="Username">
+                    <input class="w-[389px] h-[65px] bg-slate-100 rounded-[10px] shadow border-2 border-slate-100" type="text" placeholder="Password">
+                    <button class="w-[389px] h-[65px] bg-indigo-800 rounded-[10px] shadow text-white text-2xl font-bold font-['Inter']">Log in</button>
+                  <p>Don't have an account? <a on:click={toggleView}>Switch to Signup</a></p>
+                </div>
+              {/if}
+            </div>
+        </div>  
+     </div>
     </div>
   </main>
 
