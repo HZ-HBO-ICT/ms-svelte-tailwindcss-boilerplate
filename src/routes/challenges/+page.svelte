eaadd56d7814c6598e7b9462785dcd740da2b785
Jens Joosse <joos0067@hz.nl>
2024-01-10 15:56:38 +0100

    add challenges

--- src/routes/challenges/+page.svelte
+++ src/routes/challenges/+page.svelte
@@ -1,12 +1,12 @@
 <script>
     let dailyChallengeData = [
-      { day: 1, challenge: [1, 2, 3] },
-      { day: 2, challenge: [4] },
-      { day: 3, challenge: [5] },
-      { day: 4, challenge: [6] },
-      { day: 5, challenge: [7] },
-      { day: 6, challenge: [8] },
-      { day: 7, challenge: [9] },
+      { day: 1, challengeNumber: [1, 2, 3], challenge: "take a walk", },
+      { day: 2, challengeNumber: [4] , challenge: "tell your match a joke"},
+      { day: 3, challengeNumber: [5] , challenge: "Tell yourself you are beautifull in the mirror"},
+      { day: 4, challengeNumber: [6] , challenge: "hang out with someone"},
+      { day: 5, challengeNumber: [7] , challenge: "take some drugs"},
+      { day: 6, challengeNumber: [8] , challenge: "smoke a joint"},
+      { day: 7, challengeNumber: [9] , challenge: "take a yoga session"},
     ];
   </script>
   
@@ -18,17 +18,17 @@
     </div>
     <div class="content">
       <div class="w-full h-full flex items-center justify-center mt-8">
-        <div class="w-4/5 h-full bg-white">
-          {#each dailyChallengeData as { day, challenge }}
+        <div class="w-4/5 max-w-5xl h-full bg-white">
+          {#each dailyChallengeData as { day, challengeNumber, challenge }}
             <div class="m-5 bg-zinc-300 justify-center mb-10 pb-1 text-white">
               <div class="w-full h-5 bg-indigo-800">
                 Day {day}
               </div>
               <div class="justify-center">
-                {#each challenge as num}
+                {#each challengeNumber as num}
                   <div class="bg-indigo-500 h-16 m-3">
                     <div class="p-2 flex w-full">
-                        <div class="flex w-3/12">
+                        <div class="flex">
                             <div class="Rectangle72 w-12 h-12 mr-2 bg-white rounded-2xl text-black text-3xl font-bold flex justify-center">
                                 <div class="p-1">{num}</div>
                             </div>
@@ -37,9 +37,9 @@
                             </div>
                         </div>
                       <div class="w-full text-black m-1 p-1 bg-white bg-opacity-50 rounded-2xl">
-                        challenge
+                        {challenge}
                       </div>
-                      <div class="w-3/12">
+                      <div class="">
                         <div class="flex float-right">
                             <div class="Rectangle72 w-12 h-12 mr-2 bg-white rounded-2xl text-black text-3xl font-bold flex justify-center">
                                 <div class="p-1"></div>
