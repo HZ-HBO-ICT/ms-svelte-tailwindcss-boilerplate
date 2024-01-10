d7b2997f224d0089a7e1289de1d4a0748d72dfcb
Jens Joosse <joos0067@hz.nl>
2024-01-10 12:51:30 +0100

    removed code duplication and updated the looks

--- src/routes/challenges/+page.svelte
+++ src/routes/challenges/+page.svelte
@@ -1,47 +1,47 @@
 <script>
-
-</script>
-<body class="bg-slate-100 w-full h-full items-center">
+    let dailyChallengeData = [
+      { day: 1, number: [1, 2, 3] },
+      { day: 2, number: [4] },
+      { day: 3, number: [5] },
+      { day: 4, number: [6] },
+      { day: 5, number: [7] },
+      { day: 6, number: [8] },
+      { day: 7, number: [9] },
+    ];
+  </script>
+  
+  <body class="bg-slate-100 w-full h-full items-center">
     <div class="navbar">
       <div class="w-full h-24 bg-indigo-800 shadow">
         <div class="w-48 h-14 text-white text-5xl font-extrabold font-inter mx-auto">Anxietr</div>
-      </div> 
+      </div>
     </div>
     <div class="content">
-        <div class="w-full h-full flex items-center justify-center mt-8">
-            <div class="w-4/5 h-4/5 bg-white">
-                <div class="flex justify-center">
-                    <div class="h-1/4 font-inter text-3xl">
-                        challenges
+      <div class="w-full h-full flex items-center justify-center mt-8">
+        <div class="w-4/5 h-full bg-white">
+          {#each dailyChallengeData as { day, number }}
+            <div class="m-5 bg-zinc-300 justify-center mb-10 pb-1 text-white">
+              <div class="w-full h-5 bg-indigo-800">
+                Day {day}
+              </div>
+              <div class="justify-center">
+                {#each number as num}
+                  <div class="bg-indigo-500 h-16 m-3">
+                    <div class="p-2">
+                      <div class="Rectangle72 w-12 h-12 bg-white rounded-2xl text-black text-3xl font-bold flex justify-center">
+                        <div class="p-1">{num}</div>
+                      </div>
                     </div>
-                </div>
-                <div class="w-full bg-indigo-800 h-12 text-white">
-                    Todays date ------------- finished?
-                </div>
-                <div class="m-5 bg-slate-200 justify-center flex mb-10 text-white">
-                    
-                    <div class="w-full bg-indigo-800 h-5 ">
-                        Day 1
-                    
-                    <div class="w-full bg-blue-100 justify-center">
-                        <div class="bg-indigo-500  h-16 m-3">
-                            challenge 1
-                        </div>
-                        <div class="bg-indigo-500  h-16 m-3">
-                            challenge 2
-                        </div>
-                        <div class="bg-indigo-500 h-16 m-3">
-                            challenge 3
-                        </div>
-                    </div>
-                    
-                </div>
-                </div>
-            
+                  </div>
+                {/each}
+              </div>
             </div>
+          {/each}
         </div>
+      </div>
     </div>
   </body>
   
-<style>
-</style>
+  <style>
+  </style>
+  
