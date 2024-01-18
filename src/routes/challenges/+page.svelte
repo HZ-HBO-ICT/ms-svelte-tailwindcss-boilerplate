d43ef9810de230eb323f490a7975e322fdfb7af9
Ivo Petkov <112830070+1vogada@users.noreply.github.com>
2024-01-18 15:17:02 +0100

    fixed some errors

---
+++ src/routes/challenges/+page.svelte
@@ -0,0 +1,133 @@
+<script>
+  import { onMount } from "svelte";
+
+  const apiUrl = 'http://localhost:3001/getChallenges'
+  let apiChallengeData = []
+
+  onMount(async () => {
+  try {
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    apiChallengeData = await response.json();
+    console.log(apiChallengeData); 
+  } catch (error) {
+    console.error('Error:', error);
+  }
+});
+  </script>
+  
+  <body class="bg-slate-100 w-full h-full items-center">
+    <div class="navbar">
+      <div class="w-full h-24 bg-indigo-800 shadow">
+        <div class="w-48 h-14 text-white text-5xl font-extrabold font-inter mx-auto">Anxietr</div>
+      </div>
+    </div>
+    <div class="content">
+      <div class="w-full h-full flex items-center justify-center mt-8">
+        <div class="w-4/5 max-w-5xl h-full bg-white items-center">
+          <div class="text-black text-5xl font-bold font-inter text-center">Challenges</div>
+          {#if Array.isArray(apiChallengeData)}
+          <div class="m-5 bg-zinc-300 justify-center mb-10 pb-1 text-white">
+              <div class="w-full flex bg-indigo-800">
+                <div class="pl-3 h-5 text-sm font-bold">
+                  Day 1
+                </div>
+                <div class="flex justify-end">
+                  <div class="pl-3 h-5 text-sm font-bold flex">
+                    You
+                  </div>
+                  <div class="pl-3 h-5 text-sm font-bold right-3 flex ">
+                    Stranger
+                  </div>
+                </div>
+              </div>
+              {#each apiChallengeData.slice(0, 3) as { id, challenge, icon }}
+              <div class="justify-center">
+                <div class="bg-indigo-500 h-16 m-3">
+                  <div class="p-2 flex w-full">
+                    <div class="flex">
+                      <div class="Rectangle72 w-12 h-12 mr-2 bg-white rounded-2xl text-black text-3xl font-bold flex justify-center">
+                        <div class="p-1">{id}</div>
+                      </div>
+                      <div class="Rectangle72 w-12 h-12 bg-white rounded-2xl text-black text-3xl font-bold flex justify-center">
+                        <div class="p-1">{icon}</div>
+                      </div>
+                    </div>
+                    <div class="w-full text-black m-1 p-1 bg-white bg-opacity-50 rounded-2xl">
+                      {challenge}
+                    </div>
+                    <div class="">
+                      <div class="flex float-right">
+                        <div class="Rectangle72 w-12 h-12 mr-2 bg-white rounded-2xl text-black text-3xl font-bold flex justify-center">
+                          <div class="p-1"></div>
+                        </div>
+                        <div class="Rectangle72 w-12 h-12 bg-white rounded-2xl text-black text-3xl font-bold flex justify-center">
+                          <div class="p-1"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/each}
+            </div>
+          
+
+          {#each apiChallengeData.slice(3, 7) as { id, challenge, icon }}
+            <div class="m-5 bg-zinc-300 justify-center mb-10 pb-1 text-white">
+              <div class="w-full flex bg-indigo-800">
+                <div class="pl-3 h-5 text-sm font-bold">
+                  Day {id}
+                </div>
+                <div class="flex justify-end">
+                  <div class="pl-3 h-5 text-sm font-bold flex">
+                    You
+                  </div>
+                  <div class="pl-3 h-5 text-sm font-bold right-3 flex ">
+                    Stranger
+                  </div>
+                </div>
+              </div>
+              <div class="justify-center">
+                <div class="bg-indigo-500 h-16 m-3">
+                  <div class="p-2 flex w-full">
+                    <div class="flex">
+                      <div class="Rectangle72 w-12 h-12 mr-2 bg-white rounded-2xl text-black text-3xl font-bold flex justify-center">
+                        <div class="p-1">{id}</div>
+                      </div>
+                      <div class="Rectangle72 w-12 h-12 bg-white rounded-2xl text-black text-3xl font-bold flex justify-center">
+                        <div class="p-1">{icon}</div>
+                      </div>
+                    </div>
+                    <div class="w-full text-black max-h-16 m-1 p-0.5 bg-white bg-opacity-50 rounded-2xl">
+                      {challenge}
+                    </div>
+                    <div class="">
+                      <div class="flex float-right">
+                        <div class="Rectangle72 w-12 h-12 mr-2 bg-white rounded-2xl text-black text-3xl font-bold flex justify-center">
+                          <div class="p-1"></div>
+                        </div>
+                        <div class="Rectangle72 w-12 h-12 bg-white rounded-2xl text-black text-3xl font-bold flex justify-center">
+                          <div class="p-1"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          {/each}
+          {/if }
+            
+        </div>
+      </div>
+    </div>
+  </body>
+  
+  <style>
+  </style>
+  
--- src/routes/profilepage/+page.svelte
+++ src/routes/profilepage/+page.svelte
@@ -197,28 +197,28 @@
                 <div class="grid gap-4 mb-4 grid-cols-2">
                     <div class="col-span-2">
                       <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Showable Name*</label>
-                      <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
+                      <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required>
                     </div>
                     <div class="col-span-2">
                       <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Country</label>
-                      <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
+                      <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required>
                     </div>
                     <div class="col-span-2">
                       <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Open to talk about</label>
-                      <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
+                      <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required>
                     </div>
                     <div class="col-span-2">
                       <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Languages</label>
-                      <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
+                      <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required>
                     </div>
                     <div class="col-span-2">
                       <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Hobbies</label>
-                      <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
+                      <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required>
                     </div>
                     <div class="col-span-2 sm:col-span-1">
                         <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
                         <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                            <option selected="">Select category</option>
+                            <option >Select category</option>
                             <option value="Talk">Talk about problems</option>
                             <option value="PC">Tips</option>
                             
